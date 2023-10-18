f9189b5f4925d12392df3d62a7650fd7276a6d4b
hk-bui <bui.h.kien@gmail.com>
2023-10-18 17:24:03 +0200

    Update f_make_coef.m

--- src/mattools/f_make_coef.m
+++ src/mattools/f_make_coef.m
@@ -25,7 +25,7 @@ f = [];
 depend_on = [];
 varargin_list = {};
 % -
-coef_type = [];
+coef_type = 'single';
 coef_size = [];
 input_size = [];
 
@@ -35,14 +35,16 @@ valid_depend_on = {'celem','cface', ...
     'bs','js','hs','ps','as','phis','ts','omes','temps'};
 
 % --- valid coef_size
-valid_coef_type = { '1 x 1', '1', 'scalar', ...
-                    '3 x 1', '1 x 3', 'vector', ...
-                    'nb_elem x 1', 'n x 1', 'array_of_scalar', ...
-                    'nb_elem x 3', 'n x 3', 'array_of_vector', ...
-                    '3 x 3', 'gtensor', ...
-                    'nb_elem x 3 x 3', 'n x 3 x 3', 'array_of_gtensor', ...
-                    '1 x ltensor', 'ltensor', ...
-                    'nb_elem x ltensor', 'n x ltensor', 'array_of_ltensor'};
+% valid_coef_type = { '1 x 1', '1', 'scalar', ...
+%                     '3 x 1', '1 x 3', 'vector', ...
+%                     'nb_elem x 1', 'n x 1', 'array_of_scalar', ...
+%                     'nb_elem x 3', 'n x 3', 'array_of_vector', ...
+%                     '3 x 3', 'gtensor', ...
+%                     'nb_elem x 3 x 3', 'n x 3 x 3', 'array_of_gtensor', ...
+%                     '1 x ltensor', 'ltensor', ...
+%                     'nb_elem x ltensor', 'n x ltensor', 'array_of_ltensor'};
+
+valid_coef_type = { 'single','array'};
 
 % --- check and update input
 for i = 1:length(varargin)/2
@@ -63,10 +65,15 @@ else
     end
 end
 
+if ~isempty(varargin_list)
+    if mod(length(varargin_list),2) ~= 0
+        error([mfilename ' : #varargin_list must be given as cell and in pairs !']);
+    end
+end
 %------------------------------------------------------------------
 if isempty(coef_type)
-    %error([mfilename ': #coef_type must be given !']);
-elseif ~isa(aa,'char')
+    error([mfilename ': #coef_type must be given !']);
+elseif ~isa(coef_type,'char')
     error([mfilename ': #coef_type must be given as char with single quote !']);
 elseif ~any(strcmpi(strrep(coef_type,' ',''),strrep(valid_coef_type,' ','')))
     error([mfilename ': #coef_type must be one of these type : ' strjoin(valid_coef_type,', ') ' !']);
@@ -106,5 +113,29 @@ coef.varargin_list = varargin_list;
 coef.coef_type  = coef_type;
 coef.coef_size  = coef_size;
 coef.input_size = input_size;
+% --- Log message
+vl = {};
+if ~isempty(varargin_list)
+    for i = 1:length(varargin_list)/2
+        vli = varargin_list{2*i - 1};
+        if isnumeric(vli)
+            vl{i} = num2str(vli);
+        elseif ischar(vli)
+            vl{i} = vli;
+        elseif isa(vli,'function_handle') || isa(vli,'scatteredInterpolant')
+            vl{i} = char(vli);
+        else
+            vl{i} = '';
+        end
+    end
+end
+% ---
+fprintf(['Make coef with f:' char(f) ...
+         ', depend_on:' strjoin(depend_on,' ') ...
+         ', varargin_list:' strjoin(vl,' ') ...
+         ', coef_type:' coef_type '\n']);
 
 
+     
+     
+     
