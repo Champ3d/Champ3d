cf18a693f8af88b8bc431a4dab3437f3f6b8f7ab
hk-bui <bui.h.kien@gmail.com>
2023-10-25 17:37:39 +0200

    Update f_add_bsfield.m

--- src/cobjtools/f_add_bsfield.m
+++ src/cobjtools/f_add_bsfield.m
@@ -11,14 +11,14 @@ function c3dobj = f_add_bsfield(c3dobj,varargin)
 
 % --- valid argument list (to be updated each time modifying function)
 arglist = {'id_emdesign3d','id_bsfield','id_dom3d',...
-           'bs_value','bs_dir','bs_array'};
+           'bs_value','bs_dir','bs'};
 
 % --- default input value
 id_emdesign3d = [];
 id_dom3d      = [];
 bs_value      = 0;
 bs_dir        = [];
-bs_array      = [];
+bs            = [];
 id_bsfield    = [];
 
 %--------------------------------------------------------------------------
@@ -53,7 +53,7 @@ c3dobj.emdesign3d.(id_emdesign3d).bsfield.(id_bsfield).id_emdesign3d = id_emdesi
 c3dobj.emdesign3d.(id_emdesign3d).bsfield.(id_bsfield).id_dom3d = id_dom3d;
 c3dobj.emdesign3d.(id_emdesign3d).bsfield.(id_bsfield).bs_value = bs_value;
 c3dobj.emdesign3d.(id_emdesign3d).bsfield.(id_bsfield).bs_dir   = bs_dir;
-c3dobj.emdesign3d.(id_emdesign3d).bsfield.(id_bsfield).bs_array = bs_array;
+c3dobj.emdesign3d.(id_emdesign3d).bsfield.(id_bsfield).bs       = bs;
 % --- status
 c3dobj.emdesign3d.(id_emdesign3d).bsfield.(id_bsfield).to_be_rebuilt = 1;
 % --- info message
