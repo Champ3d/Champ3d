3775bd5b6041e2a7e908da572024b7ded5ef4640
hk-bui <62320126+hk-bui@users.noreply.github.com>
2023-10-26 17:07:39 +0200

    Update f_add_emdesign3d.m

--- src/cobjtools/f_add_emdesign3d.m
+++ src/cobjtools/f_add_emdesign3d.m
@@ -11,16 +11,18 @@ function c3dobj = f_add_emdesign3d(c3dobj,varargin)
 
 % --- valid argument list (to be updated each time modifying function)
 arglist = {'id_mesh3d','id_emdesign3d',...
-           'em_model','frequency'};
+           'em_model','frequency', 'from'};
 
 % --- default input value
 id_mesh3d = [];
 id_emdesign3d = [];
-em_model = 'aphijw'; % aphijw, aphits, tomejw, tomets;
+em_model = 'fem_aphijw'; % fem_aphijw, fem_aphits, fem_tomejw, fem_tomets;
 frequency = 0;
+from = [];
 
 % --- valid em_model
-valid_em_model = {'aphijw', 'aphits', 'tomejw', 'tomets'};
+valid_em_model = {'fem_aphijw', 'fem_aphits', 'fem_tomejw', 'fem_tomets', ...
+                  'fem_bem_aphijw', 'fem_bem_aphits', 'fem_bem_tomejw', 'fem_bem_tomets'};
 
 % --- check and update input
 for i = 1:length(varargin)/2
@@ -52,26 +54,48 @@ if ~any(strcmpi(em_model,valid_em_model))
     error([mfilename ': #em_model ' em_model ' is not valid. Chose : ' strjoin(valid_em_model,', ') ' !']);
 end
 %--------------------------------------------------------------------------
-switch em_model
-    case {'aphijw'}
-        formulation = 'aphi';
-        model_type = 'frequency_domain';
-    case {'aphits'}
-        formulation = 'aphi';
-        model_type = 'time_domain';
-    case {'tomejw'}
-        formulation = 'tome';
-        model_type = 'frequency_domain';
-    case {'tomets'}
-        formulation = 'tome';
-        model_type = 'time_domain';
+if any(strcmpi(em_model,{'fem_bem_aphijw', 'fem_bem_aphits', 'fem_bem_tomejw', 'fem_bem_tomets'}))
+    if isempty(from)
+        error([mfilename ': #from must be given !']);
+    end
 end
 %--------------------------------------------------------------------------
-c3dobj.emdesign3d.(id_emdesign3d).id_mesh3d   = id_mesh3d;
-c3dobj.emdesign3d.(id_emdesign3d).em_model    = em_model;
-c3dobj.emdesign3d.(id_emdesign3d).formulation = formulation;
-c3dobj.emdesign3d.(id_emdesign3d).model_type  = model_type;
-c3dobj.emdesign3d.(id_emdesign3d).frequency   = frequency;
+if ~isempty(from)
+    from__ = f_to_scellargin(from);
+    id_emdesign__ = fieldnames(c3dobj.emdesign3d);
+    for i = 1:length(from__)
+        if ~any(strcmpi(from__{i},id_emdesign__))
+            error([mfilename ': #id_emdesign3d ' from__{i} ' is not valid. Chose : ' strjoin(id_emdesign__,', ') ' !']);
+        end
+    end
+end
+%--------------------------------------------------------------------------
+switch em_model
+    case {'fem_aphijw'}
+        formulation = 'aphi';
+        model_type = 'frequency_domain';
+        discretization = 'fem';
+    case {'fem_aphits'}
+        formulation = 'aphi';
+        model_type = 'time_domain';
+        discretization = 'fem';
+    case {'fem_tomejw'}
+        formulation = 'tome';
+        model_type = 'frequency_domain';
+        discretization = 'fem';
+    case {'fem_tomets'}
+        formulation = 'tome';
+        model_type = 'time_domain';
+        discretization = 'fem';
+end
+%--------------------------------------------------------------------------
+c3dobj.emdesign3d.(id_emdesign3d).id_mesh3d      = id_mesh3d;
+c3dobj.emdesign3d.(id_emdesign3d).em_model       = em_model;
+c3dobj.emdesign3d.(id_emdesign3d).formulation    = formulation;
+c3dobj.emdesign3d.(id_emdesign3d).model_type     = model_type;
+c3dobj.emdesign3d.(id_emdesign3d).discretization = discretization;
+c3dobj.emdesign3d.(id_emdesign3d).frequency      = frequency;
+c3dobj.emdesign3d.(id_emdesign3d).from           = from;
 % ---
 c3dobj.emdesign3d.(id_emdesign3d).fields.bv    = [];
 c3dobj.emdesign3d.(id_emdesign3d).fields.jv    = [];
