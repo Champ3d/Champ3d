af2166dc81e38dc7722ff896236587265bdf9350
hk-bui <bui.h.kien@gmail.com>
2024-02-19 11:59:38 +0100

    Update FEM3dAphi.m

--- src/ch3dobj/UserEmModel/FEM3dAphi.m
+++ src/ch3dobj/UserEmModel/FEM3dAphi.m
@@ -48,10 +48,8 @@ classdef FEM3dAphi < EmModel
                     id_node_phi = f_uniquenode(elem);
                     %------------------------------------------------------
                     sigma_array = obj.(phydom_type).(id_phydom).sigma.evaluate_on(dom);
-                    det(squeeze(sigma_array(1,:,:)));
                     %------------------------------------------------------
                     sigmawewe = parent_mesh.cwewe('id_elem',gid_elem,'coefficient',sigma_array);
-                    mean(mean(mean(sigmawewe)))
                     %------------------------------------------------------
                     obj.(phydom_type).(id_phydom).matrix.id_node_phi = id_node_phi;
                     obj.(phydom_type).(id_phydom).matrix.sigmawewe = sigmawewe;
