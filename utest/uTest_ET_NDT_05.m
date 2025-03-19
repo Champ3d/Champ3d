73e438369fa0a2318ede4ffcb15d6c3590fac5cb
hk-bui <62320126+hk-bui@users.noreply.github.com>
2025-03-19 09:16:18 +0100

    .

---
+++ utest/uTest_ET_NDT_05.m
@@ -0,0 +1,16 @@
+%--------------------------------------------------------------------------
+% This code is written by: H-K. Bui, 2024
+% as a contribution to champ3d code.
+%--------------------------------------------------------------------------
+% champ3d is copyright (c) 2023 H-K. Bui.
+% See LICENSE and CREDITS files for more information.
+% Huu-Kien.Bui@univ-nantes.fr
+% IREENA Lab - UR 4642, Nantes Universite'
+%--------------------------------------------------------------------------
+
+close all
+clear all
+clc
+
+%% Parameters
+
